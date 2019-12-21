0b530ef443a7eb7a481a00b7fff76e2ec5092bfa
Terran Blake <terranblake@gmail.com>
2019-12-21 13:25:27 -0800

    remove implicit connection when importing

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,1 @@
-// async function setupReplicaSet() {
-// 	const bind_ip = 'localhost';
-// 	// Starts a 3-node replica set on ports 31000, 31001, 31002, replica set
-// 	// name is "rs0".
-// 	const replSet = new ReplSet('mongod', [
-// 		{ options: { port: 31000, dbpath: `${__dirname}/data/db/31000`, bind_ip } },
-// 		{ options: { port: 31001, dbpath: `${__dirname}/data/db/31001`, bind_ip } },
-// 		{ options: { port: 31002, dbpath: `${__dirname}/data/db/31002`, bind_ip } }
-// 	], { replSet: 'rs0' });
-
-// 	// Initialize the replica set
-// 	await replSet.purge();
-// 	await replSet.start();
-// 	console.log(new Date(), 'Replica set started...');
-// }
-
 export { EventFramework } from './classes/EventFramework';
