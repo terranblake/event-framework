604397a40b84d9c6966788df7e9fedc989c83b74
Terran Blake <terranblake@gmail.com>
2019-12-23 09:50:58 -0800

    remove unused files

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 "use strict";
 exports.__esModule = true;
+var Queue = require('bull');
+exports.Queue = Queue;
 var framework = require("./src/classes/EventFramework");
 var EventFramework = framework.EventFramework;
 exports.EventFramework = EventFramework;
--- index.ts
+++ index.ts
@@ -1,5 +1,11 @@
+const Queue = require('bull');
 import * as framework from './src/classes/EventFramework';
+
+import { Subscription } from './src/interfaces/ISubscription';
 const { EventFramework } = framework;
 
-export { EventFramework };
-export { Subscription } from './src/interfaces/ISubscription';
+export {
+	EventFramework,
+	Queue,
+	Subscription
+};
